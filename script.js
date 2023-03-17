3d7a8213c23f963a4fb0d518a0823adf91e56fc4
2Meng <tyan1401@gmail.com>
2023-03-17 16:16:38 -0500

    added function to choose specific characters

--- script.js
+++ script.js
@@ -3,9 +3,43 @@ var numberCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 var lowerCaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 var upperCaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 
+function passwordOptions(){
+  var length = promt("How many characters would you like in your password?")
 
+  if (Number.isNaN(length)){
+    alert("Please provide a number.")
+    return null
+  }
 
+  if (length < 8) {
+    alert("Password must be at least 8 characters.")
+    return null
+  }
 
+  if (length > 28) {
+    alert("Password can not be more than 28 characters.")
+  }
+
+  var useSpecialCharacters = confirm ("Click the OK button to include special characters.")
+  var useNumberCharacters = confirm ("Click the OK button to include special characters.")
+  var useLowerCaseLetters = confirm ("Click the OK button to include lower case letters.")
+  var useUpperCaseLetters = confirm ("Click the OK button to include upper case letters.")
+
+  if (useSpecialCharacters === false && useNumberCharacters === false && useLowerCaseLetters === false && useUpperCaseLetters === false) {
+    alert("Please choose AT LEAST ONE character type.")
+    return null
+  }
+
+  var usersPassword = {
+    length: length,
+    useSpecialCharacters: useSpecialCharacters,
+    useNumberCharacters: useNumberCharacters,
+    useLowerCaseLetters: useLowerCaseLetters,
+    useUpperCaseLetters: useUpperCaseLetters,
+  }
+
+  return usersPassword
+}
 
 
 
